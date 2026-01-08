6b3b075ce35259cc1ba36d39c92fd69ea9505996
mdmitkar <mitkarmuhammad@gmail.com>
2026-01-08 13:55:47 +0530

    merged loaders

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import WhatsAppButton from './components/WhatsAppButton'
-import Loader from './components/Loader'
 import LiquidFilters from './components/LiquidFilters'
 import { motion, AnimatePresence } from 'framer-motion'
-
 import BackgroundMusic from './components/BackgroundMusic'
 
 const Layout = () => {
-    const [loading, setLoading] = useState(true)
     const location = useLocation()
 
     // Scroll to top on route change
@@ -20,33 +17,28 @@ const Layout = () => {
 
     return (
         <>
-            <AnimatePresence>
-                {loading && <Loader key="loader" onComplete={() => setLoading(false)} />}
-            </AnimatePresence>
-
             {/* Invisible SVG Filters for Liquid Effects */}
             <LiquidFilters />
-            {!loading && (
-                <div className="flex flex-col min-h-screen">
-                    <Navbar />
-                    <AnimatePresence mode='wait'>
-                        <motion.main
-                            key={location.pathname}
-                            className="grow min-h-screen"
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.5 }}
-                        >
-                            <Outlet />
-                        </motion.main>
-                    </AnimatePresence>
-                    <WhatsAppButton />
-                    {/* Background Music Player */}
-                    <BackgroundMusic />
-                    <Footer />
-                </div>
-            )}
+
+            <div className="flex flex-col min-h-screen">
+                <Navbar />
+                <AnimatePresence mode='wait'>
+                    <motion.main
+                        key={location.pathname}
+                        className="grow min-h-screen"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <Outlet />
+                    </motion.main>
+                </AnimatePresence>
+                <WhatsAppButton />
+                {/* Background Music Player */}
+                <BackgroundMusic />
+                <Footer />
+            </div>
         </>
     )
 }
