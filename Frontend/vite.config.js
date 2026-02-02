78451950dd7f458ce68cf61a2369b0250b94f558
MohammadM01 <mullamohammad123@gmail.com>
2026-02-03 03:38:22 +0530

    pata nhi kuch to kiya hu agar ab bhi nhi hua to code me kuch prob h baki sab set h

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // Base URL for production deployment
+  base: "/", // root domain SEO
   build: {
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: false,
-    minify: "terser",
   },
 });
